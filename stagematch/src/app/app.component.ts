b14b71733dc2095ec8aa92455d0e2acc998e9984
manuelsahetapy <m_sahetapy@outlook.com>
2019-12-09 23:50:04 +0100

    added angular and made login screen for desktop

---
+++ stagematch/src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component, HostBinding } from '@angular/core';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+  // ...
+} from '@angular/animations';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'stagematch';
+}
